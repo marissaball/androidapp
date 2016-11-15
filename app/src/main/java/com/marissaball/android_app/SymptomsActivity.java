b65e85e942e244ca42ad24b7ed959b6d4596e2f8
Marissa Ball <marissaball614@gmail.com>
2016-11-15 13:44:27 -0500

    Added an array adapter

--- app/src/main/java/com/marissaball/android_app/SymptomsActivity.java
+++ app/src/main/java/com/marissaball/android_app/SymptomsActivity.java
@@ -4,7 +4,9 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.LinearLayout;
+import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -21,7 +23,8 @@ public class SymptomsActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_symptoms);
 
-        //Create an array of words
+
+        //Create a list of words
         ArrayList<String> symptoms = new ArrayList<String>();
 
         symptoms.add("Dizziness");
@@ -33,18 +36,23 @@ public class SymptomsActivity extends AppCompatActivity {
         symptoms.add("Tinnitus");
 
 
-        //Find the root view of the whole layout
-        LinearLayout rootView = (LinearLayout)findViewById(R.id.rootView);
+        // Create an {@link ArrayAdapter}, whose data source is a list of Strings. The
+        // adapter knows how to create layouts for each item in the list, using the
+        // simple_list_item_1.xml layout resource defined in the Android framework.
+        // This list item layout contains a single {@link TextView}, which the adapter will set to
+        // display a single word.
+        ArrayAdapter<String> itemsAdapter =
+                new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, symptoms);
 
-        for(int index = 0; index < symptoms.size(); index++){
-            //Create a new TextView
-            TextView symptomView = new TextView(this);
+        // Find the {@link ListView} object in the view hierarchy of the {@link Activity}.
+        // There should be a {@link ListView} with the view ID called list, which is declared in the
+        // activity_numbers.xml layout file.
+        ListView listView = (ListView) findViewById(R.id.list);
 
-            //Set the text to be symptom at the current index
-            symptomView.setText(symptoms.get(index));
-
-            //Add this TextView as another child to the root view of this layout
-            rootView.addView(symptomView);
-        }
+        // Make the {@link ListView} use the {@link ArrayAdapter} we created above, so that the
+        // {@link ListView} will display list items for each word in the list of words.
+        // Do this by calling the setAdapter method on the {@link ListView} object and pass in
+        // 1 argument, which is the {@link ArrayAdapter} with the variable name itemsAdapter.
+        listView.setAdapter(itemsAdapter);
     }
 }
